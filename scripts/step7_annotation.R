c29783ce05b121e0035005090f74e1d18d645f27
Selin Cildir <selincildir@gmail.com>
2024-12-18 18:20:21 +1030

    Step 7: Annotate cell types and visualize

---
+++ scripts/step7_annotation.R
@@ -0,0 +1,13 @@
+# Assigning cell type identity to clusters
+new.cluster.ids <- c("Naive CD4 T", "CD14+ Mono", "Memory CD4 T", "B", "CD8 T", "FCGR3A+ Mono",
+                     "NK", "DC", "Platelet")
+names(new.cluster.ids) <- levels(pbmc)
+pbmc <- RenameIdents(pbmc, new.cluster.ids)
+DimPlot(pbmc, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
+
+# Visualization
+library(ggplot2)
+plot <- DimPlot(pbmc, reduction = "umap", label = TRUE, label.size = 4.5) + xlab("UMAP 1") + ylab("UMAP 2") +
+  theme(axis.title = element_text(size = 18), legend.text = element_text(size = 18)) + guides(colour = guide_legend(override.aes = list(size = 10)))
+ggsave(filename = "/home/selin/Bioinformatics/pbmc_scRNAseq_analysis/results/pbmc3k_umap.jpg", height = 7, width = 12, plot = plot, quality = 50)
+
